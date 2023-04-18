1651e21c5419e1d80d39cb0c8e1bd52cf4ce744c
Vladislav Mironov <imaf6971@gmail.com>
2023-04-18 20:37:48 +0300

    fixed sdi and sdu abbr's

--- config.fish
+++ config.fish
@@ -1,14 +1,15 @@
 if status is-interactive
     colorscript random
     # Commands to run in interactive sessions can go here
-    abbr -a sdu sudo dnf update
-    abbr -a sdi sudo dnf install
+    abbr -a sdu sudo dnf update -y
+    abbr -a sdi sudo dnf install -y
     abbr -a pn pnpm
     abbr -a l lsd
     abbr -a nv nvim
     abbr -a lg lazygit
     abbr -a gits git status
     abbr -a gita git add .
+    abbr -a gitcm git commit -m
 end
 
 # pnpm
